4850616cff79989756a9f7214ae5d3169087b20d
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-08-04 14:08:15 +0200

    Swipe, frontend : Ajout des hashTag, début de CSS

--- screens/SwipeScreen.jsx
+++ screens/SwipeScreen.jsx
@@ -6,29 +6,39 @@ import SwipeButton from "../components/SwipeButton";
 export default function SwipeScreen({ navigation }) {
 	return (
 		<View style={styles.container}>
-			<Image source={require("../assets/IllustrationPorfileBase.jpg")} style={styles.image} resizeMode="cover" />
+			<View style={styles.swipeContainer}>
+				<Image source={require("../assets/IllustrationPorfileBase.jpg")} style={styles.image} resizeMode="cover" />
 
-			<View style={styles.userInformationsContainer}>
-				<Text style={styles.username}>Username</Text>
-				<Text style={styles.age}>Age</Text>
-				<Text style={styles.ville}>Ville</Text>
+				<View style={styles.textContainer}>
+					<View style={styles.userInformationsContainer}>
+						<Text style={styles.userInformation}>Username</Text>
+						<Text style={styles.userInformation}>Age</Text>
+						<Text style={styles.userInformation}>Ville</Text>
+					</View>
+
+					<View style={styles.userHashTags}>
+						<Text style={styles.hashtag}>#Violon</Text>
+						<Text style={styles.hashtag}>#Randonnée</Text>
+						<Text style={styles.hashtag}>#Chat</Text>
+					</View>
+
+					<View style={styles.choiceButtonList}>
+						<SwipeButton style={styles.choiceButton} type="Like" />
+						<SwipeButton style={styles.choiceButton} type="Superlike" />
+						<SwipeButton style={styles.choiceButton} type="Dislike" />
+					</View>
+				</View>
 			</View>
-
-			<View style={styles.userHashTags}>
-				<Text style={styles.username}>Username</Text>
-				<Text style={styles.age}>Age</Text>
-				<Text style={styles.ville}>Ville</Text>
-			</View>
-
-			<SwipeButton style={styles.information} type="Like" />
-			<SwipeButton style={styles.information} type="Superlike" />
-			<SwipeButton style={styles.information} type="Dislike" />
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
+		flex: 1,
+		backgroundColor: "#FFF5F0",
+	},
+	swipeContainer: {
 		flex: 1,
 	},
 	userInformationsContainer: {
@@ -38,10 +48,18 @@ const styles = StyleSheet.create({
 		justifyContent: "space-bettween",
 		alignItems: "center",
 	},
-	information: {
+	choiceButtonList: {
 		flex: 1,
+		flexDirection: "row",
 	},
+	userInformation: {},
 	userHashTags: {
-
+		flex: 1,
+		flexDirection: "row",
+	},
+	textContainer: {
+		flex: 1,
+		flexDirection: "colommn",
+		alignItems: "center",
 	},
 });
