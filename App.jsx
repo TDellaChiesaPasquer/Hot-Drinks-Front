c99353978741ecfb1c718d0cd5602931aab30cd5
Caroline Weinberger <caroline.weinberger04@gmail.com>
2025-08-07 16:47:42 +0200

    api geolocalisation

--- App.jsx
+++ App.jsx
@@ -39,14 +39,14 @@ const SignUpNav = () => {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false, gestureEnabled: false }}
-    >
+      >
+      <Stack.Screen name="RdvScreen" component={RdvScreen} />
       <Stack.Screen name="SignUp" component={SignUp} />
       <Stack.Screen name="DateScreen" component={DateScreen} />
       <Stack.Screen name="GenderScreen" component={GenderScreen} />
       <Stack.Screen name="RelationScreen" component={RelationScreen} />
       <Stack.Screen name="PhotoScreen" component={PhotoScreen} />
       <Stack.Screen name="MapScreen" component={MapScreen} />
-      <Stack.Screen name="RdvScreen" component={RdvScreen} />
     </Stack.Navigator>
   );
 };
--- screens/RdvScreen.jsx
+++ screens/RdvScreen.jsx
@@ -1,7 +1,28 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  StyleSheet,
+  Modal,
+  TextInput,
+  View,
+  Pressable,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import HeaderBeginning from "../components/HeaderBeginning";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useDispatch, useSelector } from "react-redux";
+import { addPlace } from "../reducers/map";
+
+
 export default function App({ navigation }) {
+
+https://us1.locationiq.com/v1/reverse?key=Your_API_Access_Token&lat=51.50344025&lon=-0.12770820958562096&format=json&
 
   return (
     <>
+     <SafeAreaView style={styles.container}>
+          <HeaderBeginning />
+          </SafeAreaView>
     </>
   );
 }
