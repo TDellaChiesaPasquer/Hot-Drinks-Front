a2921ea5af146b914b9f46de210f0ccd05dc5742
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-08-07 17:55:44 +0200

    test

--- App.jsx
+++ App.jsx
@@ -79,9 +79,7 @@ const receiveNewMessage = async (event, token, dispatch) => {
 const MainTabNav = () => {
 	const dispatch = useDispatch();
 	const user = useSelector((state) => state.user.value);
-	//   const userId = user.user._id;
-	const userId =
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODk0NzExNTA5NjdiOTE1OTgwZTRmOGMiLCJ0b2tlbk51bWJlciI6NiwiaWF0IjoxNzU0NTYwNTc0LCJleHAiOjUzNTQ1NjA1NzR9.HVQMNJzi_m6X0GuTW6DCbkEUVIzf0ZnZ_U0Zb0o1Aws";
+	const userId = user.user._id;
 	const token = user.token;
 	useEffect(() => {
 		const channel = pusher.subscribe(userId);
